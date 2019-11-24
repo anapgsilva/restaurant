318d0acaea020fdc461b59e568b7c227dd21def5
Kaitlin Culmo <culmo.kaitlin@gmail.com>
2019-11-25 10:37:05 +1100

    fix .gitignore

--- restaurant_client/src/components/SignUp.js
+++ restaurant_client/src/components/SignUp.js
@@ -24,17 +24,18 @@ class SignUp extends Component {
   _handleInputPassword = event => {
     this.setState( {password: event.target.value} )
   }
-  _handleInputPassword = event => {
+  _handleInputPhoneNumber = event => {
     this.setState( {phone_number: event.target.value} )
   }
-  _handleInputPassword = event => {
+  _handleInputAddress = event => {
     this.setState( {address: event.target.value} )
   }
 
   _handleSubmit = event => {
     event.preventDefault();
-    console.log('getting ready to be submitted');
+    // console.log('getting ready to be submitted');
     this.createUser();
+
   }
 
   createUser = event => {
@@ -50,13 +51,14 @@ class SignUp extends Component {
   render() {
     return (
       <div>
+        <h2>Sign Up</h2>
         <form onSubmit={ this._handleSubmit }>
-          <input type="text" placeholder="Name" onInput={ this._handleInputName } />
-          <input type="text" placeholder="Email" onInput={ this._handleInputEmail } />
-          <input type="text" placeholder="Password" onInput={ this._handleInputPassword } />
-          <input type="integer" placeholder="000-000-000" onInput={ this._handleInputPhone_number } />
+          <input type="text" placeholder="Name" required onInput={ this._handleInputName } />
+          <input type="text" placeholder="Email" required onInput={ this._handleInputEmail } />
+          <input type="text" placeholder="Password" required onInput={ this._handleInputPassword } />
+          <input type="text" placeholder="000-000-000" required onInput={ this._handleInputPhoneNumber } />
           <input type="text" placeholder="Address" onInput={ this._handleInputAddress } />
-          <input type="submit" placeholder="SignUp" />
+          <input type="submit" placeholder="SignUp" onClick={this._handleSubmit} />
         </form>
       </div>
     );
