e27aa002e7b8534b2074e464cc75198897230b5b
Kian <kian.info78@gmail.com>
2019-11-25 13:05:52 +1100

    nav01

--- restaurant_client/src/components/Nav.js
+++ restaurant_client/src/components/Nav.js
@@ -1,5 +1,9 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
+import logo from "../logo.png"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Button, Form, FormGroup, Label, Input,Row, Col}
+from 'react-bootstrap';
 
 class Nav extends Component {
 
@@ -17,16 +21,39 @@ class Nav extends Component {
 
   render() {
     return (
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/menu">Menu</Link>
-
-        <form>
-          <input type="text" placeholder="email"/>
-          <input type="text" placeholder="password"/>
+      <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5 ">
+        <Link to="/">
+          <img src={logo} alt="Home"
+          className="navbar-brand"/>
+        </Link>
+        <ul className="navbar-nav align-items-center">
+          <li className="nav-item ml-5">
+          <Link to="/menu" className="nav-link">Menu 
+          </Link>
+          </li>
+        </ul>
+        
+        <div className="d-flex justify-content-end">
+        <Form >
+       
+          <Row>
+          <div className="p-2">
+            <Col>
+            
+          <Form.Control type="text" placeholder="email"/>
+          
+          </Col>
+          </div>
+          <div className="p-2">
+          <Col>
+          <Form.Control type="text" placeholder="password"/>
           <Link to="/signin"><input type="submit" value="Log In" /></Link>
-        </form>
-        <Link to="/signup">Sign Up</Link>
+          </Col>
+          </div>
+          <Link to="/signup">Sign Up</Link>
+        </Row>
+        </Form>
+        </div>
 
       </nav>
     );
