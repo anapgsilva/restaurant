3a03eeb97a85fe0645227587d3797eb0ab6e16bc
Kian <kian.info78@gmail.com>
2019-11-26 15:04:36 +1100

    PaymentForn02

--- restaurant_client/src/components/CheckOut.js
+++ restaurant_client/src/components/CheckOut.js
@@ -107,7 +107,7 @@ class CheckOut extends Component {
             </div>
           </form>
 
-          {this.state.paymentOption === "Card" ? <PaymentForm onClick={this._handleCardDetails} totalPrice={30.00} orderItems={this.state.orderItems} /> : <button onClick={this.createOrder} className="pay">Pay</button>}
+          {this.state.paymentOption === "Card" ? <PaymentForm onClick={this._handleCardDetails} orderItems={this.state.orderItems} totalPrice={30} /> : <button onClick={this.createOrder} className="pay">Pay</button>}
 
         </div>
 
