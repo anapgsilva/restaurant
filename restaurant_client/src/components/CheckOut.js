d248ba79699dd05b20656296610b151fea97bf6e
Ana Silva <apgsilva81@gmail.com>
2019-11-26 12:36:38 +1100

    payment form

--- restaurant_client/src/components/CheckOut.js
+++ restaurant_client/src/components/CheckOut.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import Payment from './Payment';
+import PaymentForm from './PaymentForm';
 import {Link} from 'react-router-dom';
 import UserForm from './UserForm';
 import OrderSummary from './OrderSummary';
@@ -107,7 +107,7 @@ class CheckOut extends Component {
             </div>
           </form>
 
-          {this.state.paymentOption === "Card" ? <Payment onClick={this._handleCardDetails} totalPrice={30.00} /> : <button onClick={this.createOrder} className="pay">Pay</button>}
+          {this.state.paymentOption === "Card" ? <PaymentForm onClick={this._handleCardDetails} totalPrice={30.00} orderItems={this.state.orderItems} /> : <button onClick={this.createOrder} className="pay">Pay</button>}
 
         </div>
 
