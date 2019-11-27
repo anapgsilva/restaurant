bac5b25c407e35208875d55431079db57dea8d45
Kaitlin Culmo <culmo.kaitlin@gmail.com>
2019-11-28 08:13:20 +1100

    homepage carousel v1

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import Nav from './Nav';
+import { Carousel } from 'react-bootstrap';
+import babbo_img_1 from "../babbo_img_1.jpg"
+import babbo_img_2 from "../babbo_img_2.jpg"
+import babbo_img_3 from "../babbo_img_3.jpg"
 
 
 const Home = () => {
@@ -10,23 +14,59 @@ const Home = () => {
   // }
 
 
-
   return (
     <div>
+
       <div>
         <Nav />
       </div>
 
-      <div>
+      <Carousel>
 
-      </div>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={babbo_img_3}
+            alt="First slide"
+            />
+          <Carousel.Caption>
+            <h3>Book Your Flight With Us</h3>
+            <div className="caption-description">
+              <p>Whether you're jetting off or traveling for business, we offer flight deals to the best destinations in Australia. Find your domestic airfare, and get ready to explore the sights from sea to shining sea.</p>
+            </div>
+          </Carousel.Caption>
+        </Carousel.Item>
 
-      <div>
-        <h1>Our Restaurant</h1>
-        <p>Restaurant Info, address, phone number, photos of the restaurant, etc</p>
-      </div>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={babbo_img_1}
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>Book Your Flight With Us</h3>
+            <div className="caption-description">
+              <p>Whether you're jetting off or traveling for business, we offer flight deals to the best destinations in Australia. Find your domestic airfare, and get ready to explore the sights from sea to shining sea.</p>
+            </div>
+          </Carousel.Caption>
+        </Carousel.Item>
 
-    </div>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={babbo_img_2}
+            alt="First slide"
+          />
+        <Carousel.Caption>
+          <h3>Book Your Flight With Us</h3>
+          <div className="caption-description">
+            <p>Whether you're jetting off or traveling for business, we offer flight deals to the best destinations in Australia. Find your domestic airfare, and get ready to explore the sights from sea to shining sea.</p>
+          </div>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+
+  </div>
   );
 
 
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import logo from "../logo.png"
 import babbologo from "../babbologo.png"
 import 'bootstrap/dist/css/bootstrap.min.css';
 //import DropdownButton from 'react-bootstrap/DropdownButton';
