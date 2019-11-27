68cb6c1645f8bef13356dd31136d7dfef69f0f03
Ana Silva <apgsilva81@gmail.com>
2019-11-27 23:56:33 +1100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/restaurant_client/precache-manifest.83390fc956cf1d80b6e8a0b74ae1a224.js"
+  "/restaurant_client/precache-manifest.3028d439378a456b57355719d3551642.js"
 );
 
 self.addEventListener('message', (event) => {
