bda4361935c51517a363e85806e624a6687db437
Kaitlin Culmo <culmo.kaitlin@gmail.com>
2019-11-28 09:29:25 +1100

    carousel update

--- src/components/Home.js
+++ src/components/Home.js
@@ -29,12 +29,6 @@ const Home = () => {
             src={babbo_img_3}
             alt="First slide"
             />
-          <Carousel.Caption>
-            <h3>Book Your Flight With Us</h3>
-            <div className="caption-description">
-              <p>Whether you're jetting off or traveling for business, we offer flight deals to the best destinations in Australia. Find your domestic airfare, and get ready to explore the sights from sea to shining sea.</p>
-            </div>
-          </Carousel.Caption>
         </Carousel.Item>
 
         <Carousel.Item>
@@ -43,12 +37,6 @@ const Home = () => {
             src={babbo_img_1}
             alt="First slide"
           />
-          <Carousel.Caption>
-            <h3>Book Your Flight With Us</h3>
-            <div className="caption-description">
-              <p>Whether you're jetting off or traveling for business, we offer flight deals to the best destinations in Australia. Find your domestic airfare, and get ready to explore the sights from sea to shining sea.</p>
-            </div>
-          </Carousel.Caption>
         </Carousel.Item>
 
         <Carousel.Item>
@@ -57,12 +45,6 @@ const Home = () => {
             src={babbo_img_2}
             alt="First slide"
           />
-        <Carousel.Caption>
-          <h3>Book Your Flight With Us</h3>
-          <div className="caption-description">
-            <p>Whether you're jetting off or traveling for business, we offer flight deals to the best destinations in Australia. Find your domestic airfare, and get ready to explore the sights from sea to shining sea.</p>
-          </div>
-        </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
 
