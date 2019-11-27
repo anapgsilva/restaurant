64bc00e98349e32ceb4153cfdfd27f69bd69d4f9
Ana Silva <apgsilva81@gmail.com>
2019-11-27 19:02:59 +1100

    menu styling

--- build/precache-manifest.8650498ac262bcb7fab02674a67eb1b0.js
+++ build/precache-manifest.8650498ac262bcb7fab02674a67eb1b0.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b485aea7d24ba4a3362b8e28d2070d41",
+    "revision": "b47da7343c3d176aec1acb1756d561f8",
     "url": "/restaurant_client/index.html"
   },
   {
@@ -8,16 +8,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/restaurant_client/static/css/2.b0bb7f2c.chunk.css"
   },
   {
-    "revision": "fd27ff6d4c7bfd4e128f",
-    "url": "/restaurant_client/static/css/main.91a9883b.chunk.css"
+    "revision": "b5a19a9d83ada6bdc788",
+    "url": "/restaurant_client/static/css/main.7682a487.chunk.css"
   },
   {
     "revision": "4f862d7e3cc30616547e",
     "url": "/restaurant_client/static/js/2.7b3aed64.chunk.js"
   },
   {
-    "revision": "fd27ff6d4c7bfd4e128f",
-    "url": "/restaurant_client/static/js/main.a5e7c4f5.chunk.js"
+    "revision": "b5a19a9d83ada6bdc788",
+    "url": "/restaurant_client/static/js/main.a1ca4d83.chunk.js"
   },
   {
     "revision": "39bbd6ef5ba8ff1d6614",
