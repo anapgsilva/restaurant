a4045941b6b4f4c216a565e44a7f96b1018c3d8a
Ana Silva <apgsilva81@gmail.com>
2019-11-29 09:52:21 +1100

    deploying

---
+++ build/precache-manifest.26d1c4c7a9f5af621ed08a9e0b81253c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2264f8f6a72609535d897e3e5664124",
+    "url": "/restaurant_client/index.html"
+  },
+  {
+    "revision": "a993a61ca31297f6b262",
+    "url": "/restaurant_client/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "ef27270a30de38ecbfeb",
+    "url": "/restaurant_client/static/css/main.3c41d961.chunk.css"
+  },
+  {
+    "revision": "a993a61ca31297f6b262",
+    "url": "/restaurant_client/static/js/2.1a44c08e.chunk.js"
+  },
+  {
+    "revision": "ef27270a30de38ecbfeb",
+    "url": "/restaurant_client/static/js/main.75a60101.chunk.js"
+  },
+  {
+    "revision": "39bbd6ef5ba8ff1d6614",
+    "url": "/restaurant_client/static/js/runtime-main.6dd8802f.js"
+  },
+  {
+    "revision": "5b5f1b5628e3ea25fe0ed7a21c0b794d",
+    "url": "/restaurant_client/static/media/babbo_img_1.5b5f1b56.jpg"
+  },
+  {
+    "revision": "8023ec85c9b0ef67d04734c83684bc0b",
+    "url": "/restaurant_client/static/media/babbo_img_2.8023ec85.jpg"
+  },
+  {
+    "revision": "a51d60d8561542e3f851903e330d41ee",
+    "url": "/restaurant_client/static/media/babbo_img_3.a51d60d8.jpg"
+  },
+  {
+    "revision": "7678a3ac7056d8fa62e301fe5245d41e",
+    "url": "/restaurant_client/static/media/babbo_menu_img.7678a3ac.jpg"
+  },
+  {
+    "revision": "44abea0ac9e28c3a11875a27a1468054",
+    "url": "/restaurant_client/static/media/babbo_signup_img.44abea0a.jpg"
+  }
+]);
