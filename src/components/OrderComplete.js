8a6e6b3fc67c2e71a2253814d75c72d4c81420d6
Ana Silva <apgsilva81@gmail.com>
2019-11-29 11:18:58 +1100

    url to heroku

--- src/components/OrderComplete.js
+++ src/components/OrderComplete.js
@@ -5,8 +5,8 @@ import {Form, Col} from 'react-bootstrap';
 import axios from 'axios';
 
 
-// const SERVER_URL = "https://restaurant-order-server.herokuapp.com/users";
-const SERVER_URL = "http://localhost:3000/users";
+const SERVER_URL = "https://restaurant-order-server.herokuapp.com/users";
+// const SERVER_URL = "http://localhost:3000/users";
 // const SERVER_URL_TOKEN = "http://localhost:3000/user/token";
 const SERVER_URL_TOKEN = "https://restaurant-order-server.herokuapp.com/user/token";
 
